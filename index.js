9d91e1ab75a19aea5407f16a37275a9a492903ae
qiaoxl <qiaoxl@uw.edu>
2020-02-02 22:57:31 -0800

    First Draft Finished.

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 "use strict";
 (function() {
     const svgSize = {
-        width : 400,
-        height: 300,
+        width : 1000,
+        height: 600,
     };
-    const margin = 20;
+    const colors = {
+
+        "Bug": "#4E79A7",
+    
+        "Dark": "#A0CBE8",
+    
+        "Electric": "#F28E2B",
+    
+        "Fairy": "#FFBE7D",
+    
+        "Fighting": "#59A14F",
+    
+        "Fire": "#8CD17D",
+    
+        "Ghost": "#B6992D",
+    
+        "Grass": "#499894",
+    
+        "Ground": "#86BCB6",
+    
+        "Ice": "#FABFD2",
+    
+        "Normal": "#E15759",
+    
+        "Poison": "#FF9D9A",
+    
+        "Psychic": "#79706E",
+    
+        "Steel": "#BAB0AC",
+    
+        "Water": "#D37295"
+    
+    }
+    const margin = 30;
+    let svg = "";
 
     window.addEventListener("load", init);
 
@@ -15,44 +49,132 @@
                             .attr("height", svgSize.height + 2*margin)
                         .append("g")
                             .attr("translate", "transform(" + margin + "," + margin + ")");
-                        
+        createLegend();
         d3.csv("pokemon.csv").then((resp => makeScatterPlot(resp)));
+    }
+
+    function createLegend() {
+        let numOfType = Object.keys(colors).length;
+        for (let i = 0; i < numOfType; i++) {
+            let textContainer = gen("div");
+            textContainer.classList.add("text-div");
+            let cube = gen("div");
+            cube.classList.add("cube");
+            cube.style.background = Object.values(colors)[i];
+            textContainer.appendChild(cube);
+            let currText = Object.keys(colors)[i];
+            
+            let myText = gen("p");
+            myText.textContent = currText;
+            textContainer.appendChild(myText);
+            id("type1").appendChild(textContainer);
+        }
     }
 
     function makeScatterPlot(data) {
         let total = data.map((row) => parseInt(row["Total"]));
-        //console.log(total)
         let spdef = data.map((row) => parseInt(row["Sp. Def"]));
-        let name = data.map((row) => parseInt(row["Name"]));
-        let type1 = data.map((row) => parseInt(row["Type 1"]));
-        let type2 = data.map((row) => parseInt(row["Type 2"]));
-        //let hp = data.map((row) => parseInt(row["HP"]));
-        //let attack = data.map((row) => parseInt(row["Attack"]));
-        //let defense = data.map((row) => parseInt(row["Defense"]));
-        let generation = data.map((row) => parseInt(row["Generation"]));
-        let legendary = data.map((row) => parseInt(row["Lengendary"]));
-        //console.log(myData.name)
+        let lgd = 0;
+        let gene = 0;
+
+        /***
+         * Create x&y axis
+         */
+        let xScale = d3.scaleLinear()
+                            .domain([d3.min(spdef) - 20,d3.max(spdef) + 10])
+                            .range([0,svgSize.width - margin]);
+        svg = d3.select("svg");
+        svg.append("g")
+            .attr("transform","translate(" + margin * 2 + "," + (svgSize.height - margin) + ")")
+            .call(d3.axisBottom(xScale));
+        svg.append("text")
+            .attr("transform","translate(" + (svgSize.width/2) + "," + (margin + svgSize.height) + ")")
+            .text("Sp. Def");
+
+        let yScale = d3.scaleLinear()
+                        .domain([d3.min(total) - 50, d3.max(total) + 100])
+                        .range([svgSize.height - margin, 0]);
+        svg.append("g")
+            .attr("transform","translate(" + margin * 2 + "," + 0 + ")")
+            .call(d3.axisLeft(yScale));
+        
+        svg.append("text")
+            .attr("transform","translate(" + margin/2 + "," + (svgSize.height/2) + ") rotate(-90)")
+            .text("Total");
+        
+        
 
         /*********
          *  Create toolbar
          */
-        id("lgd").addEventListener("onchange", function() {
-            console.log(this.value);
+        drawPoints(xScale, yScale, data);
+        id("lgd").addEventListener("change", function() {
+            lgd = this.value;
+            let currData = data;
+            if (lgd == 1) {
+                currData = data.filter(function(d){ return d["Legendary"] == "True" });
+            } else if (lgd == 0) {
+                currData = data.filter(function(d){ return d["Legendary"] == "False" });
+            } else {
+                currData = data;
+            }
+            drawPoints(xScale, yScale, currData);
         });
 
-        /*let points = d3.selectAll("circle")
-                        .data(myData)
-                        .enter()
-                        .append("circle")
-                            .attr("x", function(d) {
-                                d.name;
-                                console.log(d.name)
-                            })
-                            .attr("fill", function(d) {
+        id("generation").addEventListener("change", function() {
+            gene = this.value;
+            let currData = data;
+            if (gene == -1) {
+                currData = data;
+            } else {
+                currData = data.filter(function(d){ return d["Generation"] == gene });
+            }
+            drawPoints(xScale, yScale, currData);
+        })
+    }
 
-                            });*/
+    function drawPoints(xScale, yScale, data) {
+        let allCircle = qsa("circle");
+        for (let i = 0; i < allCircle.length; i++) {
+            qs("svg").removeChild(allCircle[i]);
+        }
+        let div = d3.select("#graph-container")
+                    .append("div")
+                    .attr("class", "tooltip")
+                    .style("opacity", 0)
 
-
+        svg.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+                .attr("cx", function(d) {
+                    return xScale(d["Sp. Def"]) + margin*2;
+                })
+                .attr("cy", function(d) {
+                    return yScale(d["Total"]);
+                })
+                .attr("r", 10)
+                .attr("fill", function(d) {
+                    let t1 = d["Type 1"];
+                    return colors[t1];
+                })
+                .on("mouseover", (d) => {
+                    div.transition()
+                        .duration(200)
+                        .style("opacity", 0.9);
+                    let prompt = 'Name: ' + d['Name'] + '<br/>' + 'Type 1: ' + d['Type 1'] + '<br/>'
+                    if (d["Type 2"] != "") {
+                        prompt = prompt + "Type 2: " + d["Type 2"];
+                    }
+                    div.html(prompt)
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY + 20) + "px");
+                })
+                .on("mouseout", (d) => {
+                    div.transition()
+                        .duration(500)
+                        .style("opacity", 0)
+                })
     }
 
     /* --- HELPER FUNCTIONS --- */
